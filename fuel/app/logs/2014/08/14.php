9bf752e2c0b9074317922f91ccd4bc03a89b9bc9
alexmon1989 <alex.mon1989@gmail.com>
2014-08-14 16:16:35 +0300

    init commit 2

---
+++ fuel/app/logs/2014/08/14.php
@@ -0,0 +1,13 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-08-14 13:52:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 13:52:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 13:52:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:10:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:36:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:23:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:24:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:25:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:37:08 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:40:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-08-14 14:40:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
